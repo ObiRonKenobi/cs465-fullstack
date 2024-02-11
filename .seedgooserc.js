3eccf3c4b74d3f92cdf59c4c36bcdf4e8dbb437d
ObiRonKenobi <ronwsiii@gmail.com>
2024-02-11 12:11:54 -0500

    Created new application API

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/models',
+    modelBaseDirectory: 'app_api/models',
     models: ['*.js','!db.js'],
     data: 'data',
-    db: 'mongodb://localhost:27017/travlr'
+    db: 'mongodb://127.0.0.1/travlr'
 };
